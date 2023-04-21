026938f2e5af84e150fb727b331d44274806c178
CristobalMatus <89611143+CristobalMatus@users.noreply.github.com>
2023-04-21 15:52:08 -0400

    Add files via upload

---
+++ tarea1js.js
@@ -0,0 +1,31 @@
+const player1 = {
+    name: "Hero",
+    maxHp: 150,
+    hp: 150,
+    damage: 15
+ };
+
+ const player2 = {
+    name: "Demon",
+    maxHp: 100,
+    hp: 100,
+    damage: 25
+ };
+
+ function combat (p1,p2){
+    return p2.hp-p1.damage;
+ }
+
+ console.log("Empieza el combate!");
+
+ do {
+    console.log(player1.name +" - HP "+ player1.hp+"/"+player1.maxHp);
+    console.log(player2.name +" - HP "+ player2.hp+"/"+player2.maxHp);
+    combat(player1,player2);
+    console.log(player1.name+" deals "+player1.damage+" DMG to "+player2.name);
+    console.log(player1.name +" - HP "+ player1.hp+"/"+player1.maxHp);
+    console.log(player2.name +" - HP "+ player2.hp+"/"+player2.maxHp);
+    combat(player2,player1);
+    console.log(player2.name+" deals "+player2.damage+" DMG to "+player1.name);
+
+ } while (player1.hp !== 0 || player2.hp !==0);
